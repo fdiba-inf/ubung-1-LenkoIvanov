d030ad3954470a0a0d130bec793b0780e6f2e5d3
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-01 05:35:18 +0000

    Ubung1

--- src/exercise1/HelloFdiba.java
+++ src/exercise1/HelloFdiba.java
@@ -1,7 +1,7 @@
 package exercise1;
 
-public class HalloFdiba{
+public class HelloFdiba{
   public static void main(String [] args){
-    System.out.println("Hallo FDIBA!");
+    System.out.println("Hello FDIBA!");
   }
 }
