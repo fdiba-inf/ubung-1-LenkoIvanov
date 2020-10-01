56a32208b53b723bbb01da568f65c15ee1e27c5f
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-01 05:54:40 +0000

    ubung1

--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -4,10 +4,10 @@ public class Tree{
   public static void main(String [] args){
 
     System.out.print("*\n" );
-    System.out.print("    ***\n" );
-    System.out.print("   *****\n" );
-    System.out.print("  *******\n" );
-    System.out.print(" *********\n" );
-    System.out.print("    ***\n" );
+    System.out.print("   ***\n" );
+    System.out.print("  *****\n" );
+    System.out.print(" *******\n" );
+    System.out.print("*********\n" );
+    System.out.print("   ***\n" );
   }
 }
